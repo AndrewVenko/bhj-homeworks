30e8b49beb3ed1b917be4f81dfca3e2a1a6c134e
Andrey Vlasenko <andrewvenko95@gmail.com>
2023-02-15 18:27:55 +0700

    Fixed tasks async-request

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,11 +1,13 @@
 'use strict';
 const xhr = new XMLHttpRequest();
 const progress = document.querySelector('#progress');
-const input = document.querySelector('#file');
-
-input.addEventListener('click', function() {
-    xhr.addEventListener('readystatechange', function(){
-        xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/upload');
-        progress.value = xhr.readyState;
+const btn = document.querySelector('#send');
+btn.addEventListener('click', function() {
+    xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/upload');
+    xhr.send();
+    xhr.upload.addEventListener('progress', function(event){
+        console.log(event.total );
+        console.log(event.loaded);
+        progress.value = event.loaded;
     });
 });
