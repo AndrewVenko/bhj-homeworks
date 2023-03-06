6e6f136795911de20711ea5c2108634839def1c7
Andrey Vlasenko <andrewvenko95@gmail.com>
2023-03-06 14:12:41 +0300

    Fixed task client-state/auth

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -3,22 +3,25 @@
 const xhr = new XMLHttpRequest();
 const signin = document.querySelector('#signin');
 const signinBtn = document.querySelector('#signin__btn');
-const formSingin = document.querySelector('#signin__form');
-const welcome = document.querySelector('.#welcome');
-// задаем обработчик события для кнопки
-signinBtn.addEventListener('click', function(event){
+const formSingin = document.forms.signin__form;
+const welcome = document.querySelector('#welcome');
+// задаем обработчик события для формы
+formSingin.addEventListener('submit', function(event){
     event.preventDefault();
-    const objectAnswer = {};
     const formData = new FormData(formSingin);
     console.log(formData);
     xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth');
     xhr.send(formData);
-    xhr.onload = () => {objectAnswer = xhr.response};
-    console.log(objectAnswer.success);
-    if(objectAnswer.success === true){
-        welcome.classList.add(' welcome_active');
-        signin.classList.remove('signin_active');
-    } else{
-        alert('Ошибка авторизации!');
-    };
+    xhr.addEventListener('load', function(event){
+        let stringAnswer = xhr.response.split(',')[0].split(':')[1];
+        console.log(stringAnswer);
+        let stringId = xhr.response.split(',')[1].split(':')[1];
+        if(stringAnswer === 'true'){
+          welcome.classList.add('welcome_active');
+          welcome.textContent += stringId.substring(0, stringId.length-1);
+          signin.classList.remove('signin_active');
+        } else{
+          alert('Ошибка авторизации!');
+        };
+    });
 });
